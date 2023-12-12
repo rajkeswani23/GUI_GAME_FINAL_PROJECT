21485a2d7f2464160aea1728b0e59bc9c95f5d2c
rajkeswani <raj.keswani@menloschool.org>
2023-12-11 22:01:38 -0800

    working on card class with instance variables and setters + getters
    Created the deck class and added all of the methods

--- src/Card.java
+++ src/Card.java
@@ -1,6 +1,46 @@
 public class Card {
-    private int rank;
+    private String rank;
     private String suit;
+    private int point;
 
-    public
+    public Card(String rank, String suit, int point )
+    {
+        this.rank = rank;
+        this.suit = suit;
+        this.point = point;
+    }
+
+    public String getRank()
+    {
+        return rank;
+    }
+
+    public void setRank(String rank)
+    {
+        {
+            this.rank = rank;
+        }
+    }
+
+    public String getSuit()
+    {
+        return suit;
+    }
+
+    public int getPoint()
+    {
+        return point;
+    }
+
+    public void setPoint(int point)
+    {
+        this.point = point;
+    }
+
+    public void setSuit(String suit) {
+        if(suit.equals("clubs") || suit.equals("diamonds") || suit.equals("hearts") || suit.equals("spades"))
+        {
+            this.suit = suit;
+        }
+    }
 }
--- src/Deck.java
+++ src/Deck.java
@@ -1,3 +1,55 @@
-public class Deck {
+import java.util.ArrayList;
+;public class Deck
+{
+    private ArrayList<Card> cards;
+    private int cardsLeft;
+    public Deck(String[] ranks, String[] suits, int points[])
+    {
+        cards = new ArrayList<>();
+        int index = 0;
+        int point = points[index];
+        for (String rank : ranks)
+        {
+            for (String suit: suits)
+            {
+                cards.add(new Card(rank, suit, point));
+            }
+            index++;
+        }
+        cardsLeft = cards.size();
+        //Need to Make Method for Shuffle
+//        shuffle();
+    }
+
+    public boolean isEmpty()
+    {
+        return cardsLeft == 0;
+    }
+
+    public int getCardsLeft()
+    {
+        return cardsLeft;
+    }
+
+    public Card deal()
+    {
+        if (!isEmpty())
+        {
+            cardsLeft--;
+            return cards.get(cardsLeft);
+        }
+        return null;
+    }
+
+    public void shuffle()
+    {
+        for (int i = cards.size() - 1; i > 0; i--)
+        {
+            int r = (int)(Math.random() * (i+1));
+            Card temp = cards.get(i);
+            cards.set(i, cards.get(r));
+            cards.set(r, temp);
+        }
+    }
 
 }
